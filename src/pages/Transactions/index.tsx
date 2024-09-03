24b7770cbaf46abb54f1337b52083036b04f709c
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-09-03 11:08:40 -0300

    feat: Visual structure - Component: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 
 import * as S from './styles'
 
@@ -11,6 +12,7 @@ export function Transactions() {
       <Summary/>
 
       <S.TransactionContainer>
+        <SearchForm/>
         <S.TransactionTable>
           <tbody>
             <tr>
